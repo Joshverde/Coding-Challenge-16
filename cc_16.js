72a42ef908b35e6bcb304ea2dccabcce5e77879f
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-03-30 13:43:20 -0400

    Add fetchProductsAsync using async/await and try/catch

--- cc_16.js
+++ cc_16.js
@@ -14,3 +14,22 @@ function fetchProductThen() {
         console.error("Error:", error); // catches any erors and logs them to the console as an error
       });
   }
+//Task 3
+const fetchProducts = async function () {
+    // creates an async fucntion
+    try {
+      // trys block of code
+      const result = await fetch(
+        // waits for fetch to complete
+        "https://www.course-api.com/javascript-store-products"
+      );
+      if (!result.ok) {
+        // if the result is not ok
+        throw new Error("failed to fetch"); //  creates an error message
+      }
+      const products = await result.json(); // waits for result.json to be created
+      displayProducts(products); // calls the displayProducts function with products as the parameters
+    } catch (error) {
+      handleError(error); // calls the handle error function with the error as the parameters
+    }
+  };
